4cae6caf2b7408ffc226223cf5774f0afe98aba7
Kobala <bartlomiej.koba@gmail.com>
2020-12-11 20:19:57 +0100

    Scraps of creation

--- character_calc.py
+++ character_calc.py
@@ -1,12 +1,10 @@
-#https://pomoc.margonem.pl/index/view,3#k21
 class character:
-    def __init__(self,hp,power,agility,intelect,sa,lvl,ch):
+    def __init__(self,prof,hp,power,agility,intelect,sa,lvl,ch,armor):
+        self.prof = str(prof)
         self.hp = int(hp)
         self.power = int(power)
         self.agility = int(agility)
         self.intelect = int(intelect)
         self.sa = float(sa)
         self.ch = float(ch)
-
-class mag(character):
-
+        self.armor = int(armor)
---
+++ engine.py
@@ -0,0 +1,14 @@
+from character_clac import character
+
+class fight(character):
+    def __init__():
+        pass #need to end
+
+    def simplehit(self, attacker: character, defender: character):
+        defender.hp = defender.hp - ()
+        
+        
+
+
+
+    
